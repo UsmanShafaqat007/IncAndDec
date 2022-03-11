f3d159a9c4fd3fb430cd6e7473d1ceba8752905f
usmanshafaqat007 <usman.shafaqat@coeus-solutions.de>
2022-03-11 16:39:23 +0500

    first commit in the new branch

--- src/reducers/incAndDnc.js
+++ src/reducers/incAndDnc.js
@@ -3,10 +3,10 @@ const StartValue = 0;
 const changeNumber = (state = StartValue, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return state + 1;
+      return state + 5;
 
     case "DECREMENT":
-      return state - 1;
+      return state - 5;
 
     default:
       return state;
